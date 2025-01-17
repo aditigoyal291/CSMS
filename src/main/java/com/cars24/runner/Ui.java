305b767c23e9229e957b54626e37b68ae2b0da94
aditigoyal291 <goyaladiti2912@gmail.com>
2025-01-17 08:33:33 +0530

    Updated the code for adding the customer

--- src/main/java/com/cars24/runner/Ui.java
+++ src/main/java/com/cars24/runner/Ui.java
@@ -1,17 +1,31 @@
 package com.cars24.runner;
 import com.cars24.data.req.AddCustomerReq;
+import com.cars24.services.impl.CustomerServiceImpl;
 
 import java.util.*;
 public class Ui {
 
     private static Scanner scanner  = new Scanner(System.in);
-    public static void addCustomer()
-    {
-        System.out.println("Enter customer details");
-        AddCustomerReq req = new AddCustomerReq();
 
-        req.setName(scanner.next());
-        req.setEmail(scanner.next());
+
+    public static void addCustomer() {
+        System.out.println("Enter customer details");
+        CustomerServiceImpl customerService = new CustomerServiceImpl();
+//        AddCustomerReq addCustomerReq = null;
+
+
+        AddCustomerReq addCustomerReq = new AddCustomerReq();
+        System.out.print("Enter your name: ");
+        addCustomerReq.setName(scanner.next());
+        System.out.print("Enter your phone number: ");
+        addCustomerReq.setPhone(scanner.next());
+        System.out.print("Enter your email: ");
+        addCustomerReq.setEmail(scanner.next());
+        System.out.print("Enter your address: ");
+        addCustomerReq.setAddress(scanner.next());
+        customerService.registerCustomer(addCustomerReq);
+
+
     }
 
     public static void getCustomer()
